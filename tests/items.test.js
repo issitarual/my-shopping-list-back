017f98c3eb95d9ce1d05c69f53d5d26f32761840
Laura <laura.tracierra@gmail.com>
2021-07-12 15:37:53 -0300

    testes passando

--- tests/items.test.js
+++ tests/items.test.js
@@ -1,23 +1,45 @@
 import connection from '../src/database';
 import supertest from 'supertest';
+import app from '../src/app';
 
 beforeEach(async () => {
-    await connection.query(`DELETE FROM items WHERE name = 'teste`);
+    await connection.query(`DELETE FROM items WHERE text = 'teste'`);
   });
 
 afterAll(() => {
   connection.end();
 });
 
-describe("POST /items", async () => {
-    it("returns 200 for valid params", () => {
-        const body = {
-          text: 'teste'
-        };
-        
+describe("POST /items", () => {
+    it("returns 200 for valid params", async () => {
+        const body = { text: 'teste' };
+        const result = await supertest(app).post("/items").send(body)
+        expect(result.status).toEqual(200);
+    });
+
+    it("returns 404 for invalid params", async () => {
+        const body = { text: ' ' };
         const result = await supertest(app).post("/items").send(body);
-        const status = result.status;
-        
-        expect(status).toEqual(200);
+        expect(result.status).toEqual(404);
+    });
+
+    it("returns 404 for invalid params", async () => {
+        const body = { text: '123' };
+        const result = await supertest(app).post("/items").send(body);
+        expect(result.status).toEqual(404);
+    });
+
+    it("returns 400 for invalid params", async () => {
+        const body = { text: 'vela' };
+        const result = await supertest(app).post("/items").send(body);
+        expect(result.status).toEqual(400);
     });
 });
+
+describe("GET /items", () => {
+    it("returns 200 for valid params", async () => {
+        const result = await supertest(app).get("/items");
+        expect(result.status).toEqual(200);
+        expect(typeof result.body === 'object');
+    });
+})
